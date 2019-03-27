e3844c69931afa85b05547fe21fdebd569f199b2
kjh0202 <jin-hee01@daum.net>
2019-03-27 17:44:25 +0900

    Java

--- ch11/src/OpenChallenge_04/OC_04.java
+++ ch11/src/OpenChallenge_04/OC_04.java
@@ -1,6 +1,32 @@
 package OpenChallenge_04;
 
 import java.util.Scanner;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
 public class OC_04 {
-}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int sum = 0, square = 0;
+	 
+	Scanner scan = new Scanner(System.in);
+	 
+	System.out.print("범위를 지정하세요 : ");
+	String mixStr = scan.nextLine();
+	int mixNum = Integer.parseInt(mixStr);
+	 
+	for(int i=1; i<=mixNum; i++){
+	sum += (int)Math.pow((double)i, 2);
+	square += i;
+	}
+	 
+	square = (int)Math.pow((double)square, 2);
+	 
+	System.out.println(square + " - " + sum + " = " + (square - sum));
+	}
+	}
+
+
+
+//1~100 : 25164150
